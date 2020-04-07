41e4807372c0aa0d335e1aee80646a69aca7a663
Ajinkya Bule <54928836+AjinkyaBule@users.noreply.github.com>
2020-04-07 22:29:33 +0530

    Add files via upload

---
+++ Positiveintegersonly.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+List=[-1,3,5,-12,6,-7,8]
+print("the list with only positive inetegers :[");
+for i in List:
+    if i>=0:
+        print(i,end=",");
+
+print("]");        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
