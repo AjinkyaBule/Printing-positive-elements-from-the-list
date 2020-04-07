f5687e176793bbad83af862d5a940b6d7d129a53
Ajinkya Bule <54928836+AjinkyaBule@users.noreply.github.com>
2020-04-07 22:30:01 +0530

    Update Positiveintegersonly.py

--- Positiveintegersonly.py
+++ Positiveintegersonly.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
 List=[-1,3,5,-12,6,-7,8]
 print("the list with only positive inetegers :[");
 for i in List:
@@ -12,20 +6,6 @@ for i in List:
 
 print("]");        
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
